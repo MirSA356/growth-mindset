e4885ad9ee7f692dde7262b13eb42a2874c8befc
NASEEM AHMED <160327028+MirSA356@users.noreply.github.com>
2025-02-21 22:13:49 +0500

    Add files via upload

---
+++ app.py
@@ -0,0 +1,21 @@
+# # imports
+
+# import streamlit as st
+# import pandas as pd
+# import os
+# from io import BytesIO
+
+# st.set_page_config(page_title= "data sweeper",layout="wide" )
+
+# #custom css
+# st.markdown(
+#     """
+#     <style>
+#      .stapp{
+#             background-color: black;
+#             color: white;
+#             }
+#     </style>
+# """,
+#      unsafe_allow_html=True
+# )
